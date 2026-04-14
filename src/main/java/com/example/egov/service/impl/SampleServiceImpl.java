df2a452d698e47fa133e0c0caecc8ae82e38356e
gunwoo.park <gunwoo.park@thekeytech.co.kr>
2026-04-14 13:18:38 +0900

    주석 삭제

--- src/main/java/com/example/egov/service/impl/SampleServiceImpl.java
+++ src/main/java/com/example/egov/service/impl/SampleServiceImpl.java
@@ -44,7 +44,6 @@ public class SampleServiceImpl implements SampleService {
             ? sampleRepository.findBySjContainingAndUseAt(sj, "Y", pageable)
             : sampleRepository.findActiveNative(pageable);
         return page.map(SampleResponseDto::from);
-//        테스트용 주석
     }
 
     @Override
